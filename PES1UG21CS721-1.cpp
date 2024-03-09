7756e1388cc05357c8611f6ac7a45c13d56ae2e5
S-Vismaya <95989138+S-Vismaya@users.noreply.github.com>
2024-03-09 15:57:41 +0530

    Create PES1UG21CS721-1.cpp

---
+++ PES1UG21CS721-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, I am PES1UG21CS721\n";
+  cout << "Pipeline successful\n";
+  return 0;
+}
